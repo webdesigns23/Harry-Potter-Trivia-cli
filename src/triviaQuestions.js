b8991c8155e6fd2f179a1e25987087e768769295
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-17 21:18:36 -0600

    added trivia questions

---
+++ src/triviaQuestions.js
@@ -0,0 +1,31 @@
+const triviaQuestions = [
+    {
+        question: "What is the name of Harry Potter's owl? ",
+        options: ["1: Hedwig", "2: Helga", "3: Winky", "4: Crookshanks"],
+        correctAnswer: 1 
+    },
+    {
+        question: "Who opens the Chamber of Secrets in Harry's second year?",
+        options: ["1. Hermione Granger", "2: Collin Creevey", "3: Harry Potter", "4: Ginny Weasley"],
+        correctAnswer: 4
+    },
+    {
+        question: "Who is the Half-Blood Prince?",
+        options: ["1: Draco Malfoy ", "2: Severus Snape", "3: Ron Weasley", "4: Neville Longbottom"],
+        correctAnswer: 2
+    },
+    {
+       question: "What is the name of Hagrid's dog",
+       options: ["1: Fang", "2: Kreacher", "3: Fluffy", "4: Aragog"],
+       correctAnswer: 1
+    },
+
+    {
+        question: "Which item is a Horcrux?",
+        options: ["1: The Invisibility Cloak", "2: Nagini", "3: Elder Wand", "4: The Sorting Hat"],
+        correctAnswer: 2
+    }
+   
+];
+
+module.exports = {triviaQuestions} //Created a trivia questions module for easy update of questions
